d97388c8122aebb4780735c73ac855531cfa3864
Stefan Falkenstein <stefan@falkenste.in>
2025-07-02 22:11:15 -0400

    improved proc gen and minimap camera

--- scripts/TheCamera.gd
+++ scripts/TheCamera.gd
@@ -4,7 +4,7 @@ extends RigidBody2D
 signal position_updated(position: Vector2)
 
 func on_character_position_updated(position: Vector2) -> void:
-	const spring_constant := 10.0
+	const spring_constant := 20.0
 	const damping_constant := 2.0
 	var spring_force := -spring_constant * (global_position - position) - damping_constant * linear_velocity
 	apply_central_force(spring_force)
--- scripts/TheGenerator.gd
+++ scripts/TheGenerator.gd
@@ -4,6 +4,7 @@ extends Node2D
 # @export var additive_noise: Array[FastNoiseLite]
 
 @export var detail_noise: FastNoiseLite
+@export var size_noise: FastNoiseLite
 
 
 signal asteroid_mesh_created(mesh: Mesh)
@@ -16,32 +17,32 @@ class TileInfo:
 var generated_chunks := {}
 var generated_tiles := {}
 
-const SQUARE_CHUNK_SIZE := 512.0
+const SQUARE_CHUNK_SIZE := 4096.0
 
 # How many times the chunk is subdivided to get the fine grid
-const FINE_GRID_DEPTH := 5
+const FINE_GRID_DEPTH := 8
 
 
-func rand_point_in_rect(min: Vector2, max: Vector2) -> Vector2:
-	return Vector2(randf_range(min.x, max.x), randf_range(min.y, max.y))
+func rand_point_in_rect(rect_min: Vector2, rect_max: Vector2) -> Vector2:
+	return Vector2(randf_range(rect_min.x, rect_max.x), randf_range(rect_min.y, rect_max.y))
 
 func generate_chunk(chunk_coord: Vector2i):
 	var fine_grid_resolution = int(pow(2, FINE_GRID_DEPTH) + 0.5)
-	var fine_grid_chunk_coord = chunk_coord * fine_grid_resolution
+	# var fine_grid_chunk_coord = chunk_coord * fine_grid_resolution
 	var fine_grid_size = SQUARE_CHUNK_SIZE / fine_grid_resolution
 
-	var asteroid_spacing_factor := 1000.0
-	var max_radius := asteroid_spacing_factor / 3.7
-	var num_candidates_factor := 5
+	var asteroid_spacing_factor := 1500.0
+	var max_radius := 1500.0
+	var num_candidates_factor := 20
 
-	var rect_min :=  Vector2(chunk_coord) * SQUARE_CHUNK_SIZE
-	var rect_max := Vector2(chunk_coord + Vector2i.ONE) * SQUARE_CHUNK_SIZE
+	var rect_min :=  Vector2(chunk_coord) * SQUARE_CHUNK_SIZE + Vector2.ONE * asteroid_spacing_factor / 2
+	var rect_max := Vector2(chunk_coord + Vector2i.ONE) * SQUARE_CHUNK_SIZE - Vector2.ONE * asteroid_spacing_factor / 2
 
 	seed(hash(chunk_coord))
 
 	var initial_point := rand_point_in_rect(rect_min, rect_max)
-	var active_points = [initial_point]
-	var final_points = [initial_point]
+	var active_points := [initial_point]
+	var final_points := [initial_point]
 
 	while len(active_points) > 0:
 		var active = active_points.pop_back()
@@ -60,15 +61,18 @@ func generate_chunk(chunk_coord: Vector2i):
 				or candidate.x < rect_min.x \
 				or candidate.y > rect_max.y \
 				or candidate.y < rect_min.y:
+
 					candidate_is_valid = false
 					break
 
 			if candidate_is_valid:
+				# print_debug((candidate - active).length())
 				active_points.push_back(candidate)
 				final_points.push_back(candidate)
 
 
 
+	# print_debug(final_points)
 
 	for point in final_points:
 
@@ -82,7 +86,7 @@ func generate_chunk(chunk_coord: Vector2i):
 			var angle = angle_inc / 1000.0 * 2 * PI
 			var noise = noise_img.get_pixel(angle_inc, 0).r
 			noise = noise * 0.5 + 0.5
-			var max_distance = noise * max_radius
+			var max_distance = noise * max_radius * (size_noise.get_noise_2dv(point) + 1) / 2
 
 			for d in range(0.0, max_distance, fine_grid_size/2):
 				var new_point = d * Vector2(cos(angle), sin(angle)) + point
@@ -118,7 +122,7 @@ func generate_chunk(chunk_coord: Vector2i):
 		asteroid_body_created.emit(body)
 
 
-		body.apply_central_impulse(10 * (Vector2(randf(), randf()) * 2 - Vector2.ONE))
+		body.apply_central_impulse(100 * (Vector2(randf(), randf()) * 2 - Vector2.ONE))
 		body.apply_torque_impulse(100 * (randf() * 2 - 1))
 
 
