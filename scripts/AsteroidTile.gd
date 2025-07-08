e966ddecd4fae03f12aaa751a2fb03c83adc3448
Stefan Falkenstein <stefan@falkenste.in>
2025-07-07 22:23:17 -0400

    asteroidTile destroyed signal

--- scripts/AsteroidTile.gd
+++ scripts/AsteroidTile.gd
@@ -1,13 +1,11 @@
 class_name AsteroidTile
 extends Area2D
 
+signal destroyed()
 
 func _init() -> void:
-	add_child(CollisionShape2D.new())
-
-	var shape = get_child(0)
-	var r = RectangleShape2D.new()
-	r.size = Vector2.ONE
-	shape.shape = r
 	collision_layer = 0b1000
 	collision_mask = 0b0
+
+func emit_destroyed():
+	destroyed.emit()
