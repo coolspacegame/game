9ebc9b4902113b0e46cef97c88f566de12316c7d
Stefan Falkenstein <stefan@falkenste.in>
2025-07-10 01:28:32 -0400

    Consolidated UI scripts into one. Extracted FPS label and minimap into separate scene (along with other UI elements. Renamed TheAsteroid to Asteroid.

--- scripts/CharacterDot.gd
+++
@@ -1,6 +0,0 @@
-extends MeshInstance2D
-
-## This function is used to update the minimap player icon to the same position and rotation as that of the player character
-func _on_character_transform_updated(character_body_transform: Transform2D):
-    position = character_body_transform.get_origin()
-    rotation = character_body_transform.get_rotation()
