49bfbe2e18bd117ae4b6b992fc3e6b59588ae202
Stefan Falkenstein <stefan@falkenste.in>
2025-07-14 22:22:54 -0400

    removed spring rotation again for the camera

--- scripts/TheCamera.gd
+++ scripts/TheCamera.gd
@@ -6,7 +6,7 @@ signal position_updated(position: Vector2)
 const LINEAR_SPRING_CONSTANT := 20.0
 const LINEAR_DAMPING_CONSTANT := 2.0
 const ANGULAR_SPRING_CONSTANT := 10000.0
-const ANGULAR_DAMPING_CONSTANT := 800.0
+const ANGULAR_DAMPING_CONSTANT := 1200.0
 
 ## This function updates the camera position and rotation as if it's attached to a spring-arm connected to the character.
 ## The linear spring force and angular spring torque are calculated independently with different spring constants. 
@@ -16,16 +16,7 @@ func _on_character_transform_updated(character_body_transform: Transform2D):
 	var spring_force := -LINEAR_SPRING_CONSTANT * (global_position - pos) - LINEAR_DAMPING_CONSTANT * linear_velocity
 	apply_central_force(spring_force)
 
-	var rot := character_body_transform.get_rotation()
-	var angle_delta := global_rotation - rot
-
-	if angle_delta > PI:
-		angle_delta = angle_delta - 2 * PI
-	if angle_delta < -PI:
-		angle_delta = angle_delta + 2 * PI
-
-	var spring_torque := -ANGULAR_SPRING_CONSTANT * angle_delta - ANGULAR_DAMPING_CONSTANT * angular_velocity
-	apply_torque(spring_torque)
+	global_rotation = character_body_transform.get_rotation()
 
 func _physics_process(_delta: float) -> void:
 	position_updated.emit(position)
